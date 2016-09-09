378e302e86cf907c94005f0a1645479ef91497f5
Micah Cowell <micahcowell@gmail.com>
2016-09-08 20:19:32 -0700

    pepper season

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,2 @@
 
-//css on scroll
-$(document).scroll(function () {
-    if (window.scrollY > 40) {
-        $('.navbar').addClass('navbar__scroll');
-    }
-    else {
-        $('.navbar').removeClass('navbar__scroll');
-    }
-});
+console.log('So much POWAAHH');
